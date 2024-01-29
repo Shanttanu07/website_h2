e8b2566f8b5cdbafb403f57fd95eab343a03b649
Shanttanu07 <126013163+Shanttanu07@users.noreply.github.com>
2024-01-29 17:22:18 +0530

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -129,4 +129,16 @@ function reveal(){
     }
   })
 }
+// Initialize the gallery carousel
+$(document).ready(function () {
+    $('#gallery .carousel').slick({
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        adaptiveHeight: true,
+        prevArrow: null,
+        nextArrow: null
+    });
+});
 
